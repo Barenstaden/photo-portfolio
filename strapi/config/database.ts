42b2f6f6b4d936ef6b4adb935ba61f60911edde7
Thomas Bjørnstad <justsendto@me.com>
2024-09-22 23:01:28 +0200

    Postgres config

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -49,8 +49,13 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: {
+          ca: env('DATABASE_SSL_CA', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
             true
